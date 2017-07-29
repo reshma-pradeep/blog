9dea35b8c1050ff3e0c7842a7b365557d288ef57
reshma <reshma@debian>
2017-07-29 23:11:00 +0530

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'bcontrol/bview'
-root to: 'bcontrol#bview'
+  #get 'bcontrol/bview'
+  root to: 'bcontrol#bview'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
